d380eac62c0a7c2425944866c6f3cd7f9c2d1c74
Gabriel Moraes <gabriel.moraesdasilva14@gmail.com>
2023-03-09 01:12:59 -0300

    feat: finish header layout

--- src/components/Header/view.tsx
+++ src/components/Header/view.tsx
@@ -1,4 +1,9 @@
-import { Flex, Input, Text } from "@chakra-ui/react";
+import { Flex, Input, Text, Icon, HStack, Box, Avatar } from "@chakra-ui/react";
+import {
+  RiSearchLine,
+  RiNotificationLine,
+  RiUserAddLine,
+} from "react-icons/ri";
 
 export function HeaderView() {
   return (
@@ -38,6 +43,38 @@ export function HeaderView() {
           placeholder="Buscar na plataforma"
           _placeholder={{ color: "gray.400" }}
         />
+
+        <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gyay.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Gabriel Moraes</Text>
+            <Text color="gray.300" fontSize="small">
+              gabriel.namoraez@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar
+            size="md"
+            name="Gabriel Moraes"
+            src="https://avatars.githubusercontent.com/u/75289996?v=4"
+          />
+        </Flex>
       </Flex>
     </Flex>
   );
