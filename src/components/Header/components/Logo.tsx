a70aaf2f1fd33022e7323619881d922d653ff4af
Gabriel Moraes <gabriel.moraesdasilva14@gmail.com>
2023-03-11 02:27:18 -0300

    fix: fix logo layout

--- src/components/Header/components/Logo.tsx
+++ src/components/Header/components/Logo.tsx
@@ -1,16 +1,38 @@
-import { Text } from "@chakra-ui/react";
+import { Text, useBreakpointValue } from "@chakra-ui/react";
 
 export function Logo() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
+  if (isWideVersion) {
+    return (
+      <Text
+        fontSize={["md", "3xl"]}
+        fontWeight="bold"
+        letterSpacing="tight"
+        w="64"
+      >
+        <Text color="purple.500" as="span" ml="1">
+          Query
+        </Text>
+        Dash{" "}
+        <Text color="pink.500" as="span" ml="1">
+          _
+        </Text>
+      </Text>
+    );
+  }
+
   return (
-    <Text
-      fontSize={["md", "3xl"]}
-      fontWeight="bold"
-      letterSpacing="tight"
-      w="64"
-    >
-      Dashboard{" "}
+    <Text fontSize="4xl" fontWeight="bold" letterSpacing="tight" w="64">
+      <Text color="purple.500" as="span" ml="1">
+        q
+      </Text>
+      D{" "}
       <Text color="pink.500" as="span" ml="1">
-        .
+        _
       </Text>
     </Text>
   );
